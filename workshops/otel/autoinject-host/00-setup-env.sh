d4c7a00e3e4393688f4e23ffe2c992b9c94e0756
Scott Heinlein <scott.heinlein@coralogix.com>
2025-07-07 21:18:30 +0000

    fixed an issue with newline escapes

--- workshops/otel/autoinject-host/00-setup-env.sh
+++ workshops/otel/autoinject-host/00-setup-env.sh
@@ -30,8 +30,7 @@ echo "Setting permissions for Docker GPG key..."
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "Adding Docker APT repository to sources list..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \n  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}" | tr -d '\n') stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Updating apt package index after adding Docker repository..."
 sudo apt-get update
